42ec55b2646b71958fa79f81e8fb0f1173a2774d
Venkataraju2002 <84319124+Venkataraju2002@users.noreply.github.com>
2022-04-05 20:32:52 +0530

    def self_divide(n):     s=n     k=str(n)     count=0     while n:         d=n%10         n=n//10         if d==0:             continue
            if s%d==0:
                count=count+1
                
        if(count==len(k)):
            return True
        return False
    a=int(input())
    b=int(input())
    for i in range(a,b+1):
        if(self_divide(i)==True):
            print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,17 +1,24 @@
 def self_divide(n):
+    s=n
     k=str(n)
-    j=list(k)
     count=0
-    for i in j:
-        if(int(i)==0):
+    while n:
+        d=n%10
+        n=n//10
+        if d==0:
             continue
-        if(n%int(i)==0):
-            count+=1
-        if (count==len(k)):
-            return True
+
+        if s%d==0:
+            count=count+1
+            
+    if(count==len(k)):
+        return True
     return False
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-    if(self_divide(i)==True) :
+    if(self_divide(i)==True):
         print(i,end=" ")
+
+
+
