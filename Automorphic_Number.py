f578842d6d5b0b52f31347bc36a4ab73c7e77239
Venkataraju2002 <84319124+Venkataraju2002@users.noreply.github.com>
2022-04-18 14:20:43 +0530

    n=int(input()) k=n**2 n=str(n) k=str(k) l=len(n) j=k[-l:] if (int(j)==int(n)):     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,11 +1,10 @@
 n=int(input())
 k=n**2
 n=str(n)
-l1=len(n)
 k=str(k)
-l2=len(k)
-j = k[-l1:]
-if(int(j) == int(n)):
+l=len(n)
+j=k[-l:]
+if (int(j)==int(n)):
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
